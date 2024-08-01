b9ad2a67ca38bb20040326baaf1b183aaf02cb97
Adam <adambvista@yahoo.com>
2024-08-01 15:35:49 -0700

    added solutions to exercise 1.7 & 1.8 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -17,10 +17,7 @@ function App() {
       <Button handleClick={increaseValue(good, setGood)} text="good" />
       <Button handleClick={increaseValue(neutral, setNeutral)} text="neutral" />
       <Button handleClick={increaseValue(bad, setBad)} text="bad" />
-      <Header heading="statistics" />
-      <Display text="good" value={good} />
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
@@ -39,6 +36,32 @@ function Display({ text, value }) {
 
 function Header({ heading }) {
   return <h1>{heading}</h1>
+}
+
+function Statistics({ good, neutral, bad }) {
+  function totalFeedback() {
+    return [good, neutral, bad].reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  }
+
+  function averageFeedback() {
+    return isNaN((good - bad) / totalFeedback()) ? 0 : (good - bad) / totalFeedback()
+  }
+
+  function positiveFeedbackPercent() {
+    return isNaN(good / totalFeedback()) ? 0 : good / totalFeedback()
+  }
+
+  return (
+    <>
+      <Header heading="statistics" />
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+      <Display text="all" value={totalFeedback()} />
+      <Display text="average" value={averageFeedback()} />
+      <Display text="positive" value={positiveFeedbackPercent() + " %"} />
+    </>
+  )
 }
 
 export default App
